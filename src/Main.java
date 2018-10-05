1d892bb55795ccb0eb93d908f06ebd75851e4aad
Burhozturk <31951806+Burhozturk@users.noreply.github.com>
2018-10-05 08:23:10 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int miles[]={1,3,9,10};
+        double kilometers[]={1.609,3.218,14.481,16.090};
+        System.out.println("Miles");
+        for(int a=0;a<miles.length;a++)
+        {
+            System.out.println(miles[a]);
+        }
+        System.out.println("Kilometers");
+        for(int b=0;b<kilometers.length;b++)
+        {
+            System.out.println(kilometers[b]);
+        }
+
+    }
+}
